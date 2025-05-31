8340ddc86e437a0e407752ee63173242bc2eeb08
prashanttnegi <prashant.negi0407@gmail.com>
2025-05-31 21:01:39 +0530

    Minor changes

--- main.py
+++ main.py
@@ -78,7 +78,6 @@ prediction=mod_dt.predict(X_test)
 
 print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
 
-import pickle
 import joblib
 
 joblib.dump(mod_dt, "artifacts/model.joblib")
--- main.sh
+++ main.sh
@@ -4,8 +4,14 @@
 
 ! gsutil mb -l "us-central1" -p "sharp-cursor-461116-t1" "gs://mlops-course-sharp-cursor-461116-t1-week2"
 
-python main.py
+echo "Created bucket gs://mlops-course-sharp-cursor-461116-t1-week2"
+
+echo "Making model artifacts directory"
 
 mkdir -p artifacts
+
+python main.py
+
+echo "Uploading model artifacts to gs://mlops-course-sharp-cursor-461116-t1-week2/my-models/iris-classifier-week-2/"
 
 ! gsutil cp artifacts/model.joblib "gs://mlops-course-sharp-cursor-461116-t1-week2"/"my-models/iris-classifier-week-2"/
