492cb520243bb0ae18c6a846f7f99a4b605d0ad0
prashanttnegi <prashant.negi0407@gmail.com>
2025-05-31 19:17:21 +0530

    Updates sh file

--- main.sh
+++ main.sh
@@ -2,6 +2,6 @@
 
 ! gsutil mb -l "us-central1" -p "sharp-cursor-461116-t1" "gs://mlops-course-sharp-cursor-461116-t1-week2"
 
-python main_2.py
+python main.py
 
 !gsutil cp artifacts/model.joblib "gs://mlops-course-sharp-cursor-461116-t1-week2"/"my-models/iris-classifier-week-2"/
